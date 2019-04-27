2e7d99362c8c9f69af5248131ec847e89ee7fef1
Adam Kariv <adam.kariv@gmail.com>
2019-04-27 16:59:35 +0300

    v0.0.29 adds support for loader dgps

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def read(*paths):
 PACKAGE = 'dgp_server'
 NAME = PACKAGE.replace('_', '-')
 INSTALL_REQUIRES = [
-    'dgp>=0.0.14',
+    'dgp>=0.0.15',
     'gunicorn',
     'aiohttp',
     'aiohttp_sse',
