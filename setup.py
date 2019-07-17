64a475f68935d8ebd6fad6e295b5ae42a397eb53
Adam Kariv <adam.kariv@gmail.com>
2019-07-17 09:13:39 +0300

    v0.0.44 bump dgp to 0.0.23

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def read(*paths):
 PACKAGE = 'dgp_server'
 NAME = PACKAGE.replace('_', '-')
 INSTALL_REQUIRES = [
-    'dgp>=0.0.22',
+    'dgp>=0.0.23',
     'gunicorn',
     'aiohttp',
     'aiohttp_sse',
